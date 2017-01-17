adc3b2b654fb87fc51df3051c593a1334d557215
Chan Jun Shern <chanjunshern@gmail.com>
2017-01-17 00:17:51 +0000

    Pixel-colored triangles working but disappointing

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var img;
 var smallPoint, largePoint;
 var particles = [];
-var numParticles = 100;
+var numParticles = 30;
 
 function preload() {
   img = loadImage("assets/portrait.jpg");
@@ -19,44 +19,80 @@ function setup() {
   for (var i=0; i<numParticles; i++) {
     particles[i] = new Particle();
   }
+
+  frameRate(15); // Limit framerate for cross-device consistency
 }
 
 function draw() {
   background(255);
 
   for (var i=0; i<numParticles; i++) {
+    var x1 = particles[i].x;
+    var y1 = particles[i].y;
     particles[i].move();
     particles[i].display();
+    for (var j=i+1; j<numParticles; j++) {
+      var x2 = particles[j].x;
+      var y2 = particles[j].y;
+      //particles[j].move();
+      for (var k=j+1; k<numParticles; k++) {
+        var x3 = particles[k].x;
+        var y3 = particles[k].y;
+        //particles[k].move();
+        if (i!=j && i!=k && j!=k) {
+          // Check if particles are nearby and draw triangle if they are
+          var d = 100;
+          if (dist(x1,y1,x2,y2)<d && dist(x1,y1,x3,y3)<d && dist(x2,y2,x3,y3)<d) {
+            fill(img.get(x1, y1));
+            triangle(x1,y1,x2,y2,x3,y3);
+          } 
+        }
+
+      }
+    }
   }
+
 }
 
 function Particle(index) {
   this.x = random(width);
   this.y = random(height);
-  this.velX = random(-5,5);
-  this.velY = random(-5,5);
+  this.velX = random(-2,2);
+  this.velY = random(-2,2);
   this.diameter = 5;
   
-  this.c = color(0, 0, 0); 
+  //this.c = color(random(10,50), 100, 100); 
 
   this.display = function() {
-    //var pix = img.get(this.x, this.y);
-    //fill(pix, 128);
-    fill(this.c, 128);
+    var pix = img.get(this.x, this.y);
+    fill(pix, 128);
+    //fill(this.c, 128);
     ellipse(this.x, this.y, this.diameter, this.diameter);
   }
 
   this.move = function() {
-    if (this.x > width || this.x < 0) {
+    // Mouse attraction
+    // var pullRadius = 50;
+    // if (abs(mouseX-this.x)<pullRadius && abs(mouseY-this.y)<pullRadius) {
+    //   var pullX = constrain(mouseX-this.x, -2, 2);
+    //   var pullY = constrain(mouseY-this.y, -2, 2);
+    //   this.velX = constrain(this.velX+pullX, -5, 5);
+    //   this.velY = constrain(this.velY+pullY, -5, 5); 
+    // }
+
+    // Update
+    this.x = constrain(this.x + this.velX, 0, width);
+    this.y = constrain(this.y + this.velY, 0, height);
+
+    // Border collision
+    if (this.x >= width || this.x <= 0) {
       this.velX = -this.velX;
-      this.x = constrain(this.x, 0, width);
     }
-    if (this.y > width || this.y < 0) {
+    if (this.y >= width || this.y <= 0) {
       this.velY = -this.velY;
-      this.y = constrain(this.y, 0, width);
+      
     }
-    this.x = this.x + this.velX;
-    this.y = this.y + this.velY;
+
   }
 
   this.checkMouse = function() {
