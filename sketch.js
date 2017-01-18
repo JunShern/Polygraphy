965e2da763e4cdd94009dcf0f7af48203326d5d5
Chan Jun Shern <chanjunshern@gmail.com>
2017-01-18 01:25:48 +0000

    Watch-as-it-unfolds and added new test images

--- sketch.js
+++ sketch.js
@@ -1,15 +1,21 @@
 var img;
 var particles = [];
-var numParticles = 200;
+var numParticles = 400;
 var pixDensity;
+var i=0;
 
 function preload() {
+  //img = loadImage("assets/london.jpg");
+  //img = loadImage("assets/bird.jpg");
+  //img = loadImage("assets/hallstatt.jpg");
   img = loadImage("assets/portrait.jpg");
+
 }
 
 function setup() {
   createCanvas(img.width, img.height);
-
+  console.log(img.width * img.height);
+  //numParticles = int(img.width * img.height / 800);
   //imageMode(CENTER);
   img.loadPixels();
   pixDensity = pixelDensity();
@@ -17,51 +23,45 @@ function setup() {
   noStroke();
   //colorMode(HSB,100);
 
-  for (var i=0; i<numParticles; i++) {
-    particles[i] = new Particle();
+  for (var k=0; k<numParticles; k++) {
+    particles[k] = new Particle();
   }
 
   frameRate(15); // Limit framerate for cross-device consistency
 
-  for (var i=0; i<numParticles; i++) {
-    console.log("Particle " + i + " of " + numParticles);
-    var x1 = particles[i].x;
-    var y1 = particles[i].y;
-    particles[i].move();
-    //particles[i].display();
-    for (var j=i+1; j<numParticles; j++) {
-      var x2 = particles[j].x;
-      var y2 = particles[j].y;
-      //particles[j].move();
-      for (var k=j+1; k<numParticles; k++) {
-        var x3 = particles[k].x;
-        var y3 = particles[k].y;
-        //particles[k].move();
-        if (i!=j && i!=k && j!=k) {
-          // Check if particles are nearby and draw triangle if they are
-          var d = 100;
-          if (dist(x1,y1,x2,y2)<d && dist(x1,y1,x3,y3)<d && dist(x2,y2,x3,y3)<d) {
-            var aveX = (x1+x2+x3)/3;
-            var aveY = (y1+y2+y3)/3;
-
-            var c = img.get(aveX,aveY);
-            if (brightness(c)<70) {
-              fill(c[0],c[1],c[2],128);
-              //stroke(255);
-              //strokeWeight(0.5);
-              triangle(x1,y1,x2,y2,x3,y3);
-            }
-
-          } 
-        }
-
-      }
-    }
-  }
 }
 
 function draw() {
+  var x1 = particles[i].x;
+  var y1 = particles[i].y;
   //background(255);
+  for (var j=i+1; j<numParticles; j++) {
+    var x2 = particles[j].x;
+    var y2 = particles[j].y;
+    //particles[j].move();
+    for (var k=j+1; k<numParticles; k++) {
+      var x3 = particles[k].x;
+      var y3 = particles[k].y;
+      //particles[k].move();
+      if (i!=j && i!=k && j!=k) {
+        // Check if particles are nearby and draw triangle if they are
+        var d = (width+height)/15; //width/6;
+        if (dist(x1,y1,x2,y2)<d && dist(x1,y1,x3,y3)<d && dist(x2,y2,x3,y3)<d) {
+          var aveX = (x1+x2+x3)/3;
+          var aveY = (y1+y2+y3)/3;
+
+          var c = img.get(aveX,aveY);
+          fill(c[0],c[1],c[2],128);
+          //stroke(255);
+          //strokeWeight(0.5);
+          triangle(x1,y1,x2,y2,x3,y3);  
+        } 
+      }
+
+    }
+  }  
+  i++;
+  if (i > numParticles) noLoop(); // End the program
 }
 
 function Particle(index) {
@@ -80,37 +80,6 @@ function Particle(index) {
     //fill(pix, 128);
     fill(this.c, 128);
     ellipse(this.x, this.y, this.diameter, this.diameter);
-  }
-
-  this.move = function() {
-    // Mouse attraction
-    // var pullRadius = 50;
-    // if (abs(mouseX-this.x)<pullRadius && abs(mouseY-this.y)<pullRadius) {
-    //   var pullX = constrain(mouseX-this.x, -2, 2);
-    //   var pullY = constrain(mouseY-this.y, -2, 2);
-    //   this.velX = constrain(this.velX+pullX, -5, 5);
-    //   this.velY = constrain(this.velY+pullY, -5, 5); 
-    // }
-
-    // Update
-    this.x = constrain(this.x + this.velX, 0, width);
-    this.y = constrain(this.y + this.velY, 0, height);
-
-    // Border collision
-    if (this.x >= width || this.x <= 0) {
-      this.velX = -this.velX;
-    }
-    if (this.y >= width || this.y <= 0) {
-      this.velY = -this.velY;
-      
-    }
-
-  }
-
-  this.checkMouse = function() {
-    if (abs(this.y-mouseY)<catchSize*4 && abs(this.x-mouseX)<catchSize*4) {
-      ;
-    } 
   }
 
 }
